507a3a5ec866f6a232817fbda83fc38804b6c55c
Dmitry Kosenko <dmitrij.kosenko.98@mail.ru>
2024-05-22 12:16:04 +0700

    Добавьте файлы проекта.

---
+++ FileProcessorWindowsForm/Form1.cs
@@ -0,0 +1,116 @@
+namespace FileProcessorWindowsForm
+{
+    public partial class Form1 : Form
+    {
+        ImageList imageList1;
+        public void ShowDrives()
+        {
+            treeView1.BeginUpdate();
+            string[] drives = Directory.GetLogicalDrives();
+            foreach (string drive in drives)
+            {
+                TreeNode tn = new TreeNode(drive);
+                treeView1.Nodes.Add(tn);
+                AddDirs(tn);
+            }
+            treeView1.EndUpdate();
+        }
+
+        public void ShowFileNames()
+        {
+            DirectoryInfo di = new DirectoryInfo(treeView1.SelectedNode.FullPath);
+            FileInfo[] files = di.GetFiles();
+            ListViewItem item;
+            imageList1 = new ImageList();
+
+            listView1.Items.Clear();
+            listView1.SmallImageList = imageList1;
+
+            if (di.Exists)
+                files = di.GetFiles();
+
+            listView1.BeginUpdate();
+            foreach (FileInfo file in files)
+            {
+                Icon iconForFile;
+                item = new ListViewItem(file.Name);
+                listView1.Items.Add(item);
+                iconForFile = SystemIcons.WinLogo;
+
+                if(!imageList1.Images.ContainsKey(file.Extension))
+                {
+                    iconForFile = System.Drawing.Icon.ExtractAssociatedIcon(file.FullName);
+                    imageList1.Images.Add(file.Extension, iconForFile);
+                }
+
+                item.ImageKey = file.Extension;
+                item.SubItems.Add((file.Length/1024) + " КБ");
+                item.SubItems.Add(file.LastWriteTime.ToString());
+                item.SubItems.Add(GetAttributes(file));
+
+            }
+            listView1.EndUpdate();
+        }
+
+        private string GetAttributes(FileInfo fileInfo)
+        {
+            string attr = "";
+            
+            if ((fileInfo.Attributes & FileAttributes.Hidden) != 0)
+                attr += "Спрятанный ";
+            if ((fileInfo.Attributes & FileAttributes.ReadOnly) != 0)
+                attr += "Только чтение ";
+            if ((fileInfo.Attributes & FileAttributes.System) != 0)
+                attr += "Системный";
+            else if ((fileInfo.Extension == ".dll"))
+                attr += "Расширение";
+            else if((fileInfo.Extension == ".exe"))
+                attr += "Приложение";
+            else attr += "Файл " + fileInfo.Extension;
+            return attr;
+        }
+
+        public void AddDirs(TreeNode node)
+        {
+            DirectoryInfo di = new DirectoryInfo(node.FullPath);
+            DirectoryInfo[] dirs = { };
+
+            try
+            {
+                if (di.Exists)
+                    dirs = di.GetDirectories();
+            }
+            catch { return; }
+
+            foreach (DirectoryInfo info in dirs)
+            {
+                TreeNode treeNode = new TreeNode(info.Name);
+                node.Nodes.Add(treeNode);
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ShowDrives();
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            ShowFileNames();
+        }
+
+        private void treeView1_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            treeView1.BeginUpdate();
+            foreach(TreeNode node in e.Node.Nodes) 
+            {
+                AddDirs(node);
+            }
+            treeView1.EndUpdate();
+        }
+    }
+}
